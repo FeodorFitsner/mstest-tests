58d65d24308b1e1665f9ca218cba0428653f779e
Feodor Fitsner <feodor@fitsner.com>
2014-10-15 17:10:21 -0700

    Initial check-in

---
+++ UnitTestProject/CategorizedUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class CategorizedUnitTests
+    {
+        [TestMethod, TestCategory("A")]
+        public void Test_from_category_A()
+        {
+            var a = 3;
+            Assert.AreEqual(3, a);
+        }
+
+        [TestMethod, TestCategory("B")]
+        public void Test_from_category_B()
+        {
+            var b = 3;
+            Assert.AreEqual(3, b);
+        }
+
+        [TestMethod, TestCategory("C")]
+        public void Test_from_category_C()
+        {
+            var c = 3;
+            Assert.AreEqual(3, c);
+        }
+    }
+}
